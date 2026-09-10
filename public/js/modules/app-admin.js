573e9910d8c7b39c3cf9b7a9d84a04b80eb4bf46
birdcrazy <227081272+birdcrazy@users.noreply.github.com>
2026-09-09 21:11:42 -0400

    Add inline message text colors (#5623)
    * Add color
    
    * change color shortcut to "F" instead of "C"
    Since code blocs already use "C"
    
    * Allow multiline text coloring

--- public/js/modules/app-admin.js
+++ public/js/modules/app-admin.js
@@ -2484,6 +2484,17 @@ _handleMarkdownShortcuts(inputEl, event) {
   if (event.shiftKey && key === 'p') {
     return this._wrapSelectedText(input, '||', `||`);
   }
+
+  // Text color (Ctrl/Cmd + Shift + F) (F for font, since c for code is already taken)
+  if (event.shiftKey && key === 'f') {
+    const colorPrefix = 'c#(51,153,255)'
+    if (!this._wrapSelectedText(input, colorPrefix, '#c')) return false;
+
+    // Move cursor to just before ")" so the user can edit the color
+    const cursorPos = input.selectionStart + (colorPrefix.length - 1);
+    input.setSelectionRange(cursorPos, cursorPos);
+    return true;
+  }
   return false;
 },
 
